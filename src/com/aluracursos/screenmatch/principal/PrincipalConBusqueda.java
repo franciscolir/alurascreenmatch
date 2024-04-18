ce2b442191c5c6991a9d869bfda14fe38ebfcbfa
franciscolir <francisco.lira.b@gmail.com>
2024-04-18 12:53:00 -0400

    "Implementando API"

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -5,12 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escriba el nombre de una pelicula: ");
+        var busqueda = teclado.nextLine();
+        String direccion = "https://www.omdbapi.com/?t=" + busqueda + "&apikey=5f8f033e";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=5f8f033e"))
+                .uri(URI.create(direccion))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
