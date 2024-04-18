9c069c0ee308889eaf56b7ba07b4a1cc7f567569
franciscolir <francisco.lira.b@gmail.com>
2024-04-18 08:57:10 -0400

    "Implementando API"

---
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,20 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=5f8f033e"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
